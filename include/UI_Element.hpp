c5704766f0458829a3524ef37145e59d3682ad8a
grizeus <zaytsewww@gmail.com>
2023-06-16 00:50:01 +0300

    add some pseudo & new obj

---
+++ include/UI_Element.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <utility>
+#include <string>
+
+struct UI_Element 
+{
+    int m_Height;
+    int m_Width;
+    std::pair<float, float> m_Position;
+
+    uint64_t m_BackgroundColor;
+    uint64_t m_HoverBackgroundColor;
+    uint64_t m_BorderColor;
+    uint64_t m_ContentColor;
+
+    void Draw(std::string);
+    void Hover(bool);
+private:
+    SDL* m_drawer;
+};
---
+++ src/UI_Element.cpp
@@ -0,0 +1,12 @@
+#include "../include/UI_Element.hpp"
+
+void UI_Element::Draw(std::string string)
+{
+    // draw with ttf whenever it is
+}
+
+void UI_Element::Hover(bool IsHover)
+{
+    if (IsHover == true)
+        draw hover;
+}
