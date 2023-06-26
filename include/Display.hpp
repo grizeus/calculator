199dd1912180d133bed88b2ec42e76d48aaaa0da
grizeus <zaytsewww@gmail.com>
2023-06-26 20:18:58 +0300

    refactor Display

--- include/Display.hpp
+++ include/Display.hpp
@@ -6,6 +6,24 @@ public:
     Display(ToolsPtr drawer);
     ~Display() { }
     void Draw(const std::string&);
+
+    // UI_Element's getters and setters
+    int GetWidth() const { return m_UI_Element.GetWidth(); }
+    int GetHeight() const { return m_UI_Element.GetHeight(); }
+    Coordinate GetPosition() const { return m_UI_Element.GetPosition(); }
+    UI_Element::Color GetBackgroundColor() const { return m_UI_Element.GetBackgroundColor(); }
+    UI_Element::Color GetContentColor() const { return m_UI_Element.GetContentColor(); }
+    UI_Element::Color GetBorderColor() const { return m_UI_Element.GetBorderColor(); }
+    UI_Element::Color GetHoverColor() const { return m_UI_Element.GetHoverColor(); }
+    ToolsPtr GetTools() const { return m_UI_Element.GetTools(); }
+
+    void SetWidth(int);
+    void SetHeight(int);
+    void SetPosition(float, float);
+    void SetBackgroundColor(uint8_t, uint8_t, uint8_t, uint8_t);
+    void SetContentColor(uint8_t, uint8_t, uint8_t, uint8_t);
+    void SetBorderColor(uint8_t, uint8_t, uint8_t, uint8_t);
+    void SetTools(ToolsPtr);
 private:
-    std::shared_ptr<UI_Element> m_UI_Element;
+    UI_Element m_UI_Element;
 };
--- src/Display.cpp
+++ src/Display.cpp
@@ -2,24 +2,23 @@
 
 
 Display::Display(ToolsPtr drawer)
+    : m_UI_Element(drawer)
 {
-    std::shared_ptr<UI_Element> m_UI_Element(new UI_Element(drawer));
-    m_UI_Element->SetHeight(100);
-    m_UI_Element->SetWidth(497);
-    m_UI_Element->SetPosition(0.0f, 0.0f);
-    m_UI_Element->SetBackgroundColor(0xCF, 0xCF, 0xCF, 0xCF);
-    m_UI_Element->SetBorderColor( 0xCF, 0xCF, 0xCF, 0xFF);
-    m_UI_Element->SetHoverColor (0xCF, 0xCF, 0xCF, 0xCF);
-    m_UI_Element->SetContentColor(0x1E, 0x90, 0xFF, 0xCF); // dodger blue
+    SetHeight(100);
+    SetWidth(497);
+    SetPosition(0.0f, 0.0f);
+    SetBackgroundColor(0xCF, 0xCF, 0xCF, 0xCF);
+    SetBorderColor( 0xCF, 0xCF, 0xCF, 0xFF);
+    SetContentColor(0x1E, 0x90, 0xFF, 0xCF); // dodger blue
 }
 
 void Display::Draw(const std::string& content) {
-    SDL_Renderer* renderer = m_UI_Element->GetTools()->m_Renderer;
-    SDL_Rect DisplayRect = {static_cast<int>(m_UI_Element->GetPosition().first), static_cast<int>(m_UI_Element->GetPosition().second), m_UI_Element->GetWidth(), m_UI_Element->GetHeight()};
-    SDL_SetRenderDrawColor(renderer, m_UI_Element->GetBorderColor().red, m_UI_Element->GetBorderColor().green, m_UI_Element->GetBorderColor().blue, m_UI_Element->GetBorderColor().alpha);
+    SDL_Renderer* renderer = GetTools()->m_Renderer;
+    SDL_Rect DisplayRect = {static_cast<int>(GetPosition().first), static_cast<int>(GetPosition().second), GetWidth(), GetHeight()};
+    SDL_SetRenderDrawColor(renderer, GetBorderColor().red, GetBorderColor().green, GetBorderColor().blue, GetBorderColor().alpha);
     SDL_RenderDrawRect(renderer, &DisplayRect);
 
-    SDL_Surface* TextSurface = TTF_RenderText_Solid(m_UI_Element->GetTools()->m_Font, content.c_str(), m_UI_Element->ConvertToSDLColor(m_UI_Element->GetContentColor()));
+    SDL_Surface* TextSurface = TTF_RenderText_Solid(GetTools()->m_Font, content.c_str(), m_UI_Element.ConvertToSDLColor(GetContentColor()));
     if (TextSurface == nullptr) 
         throw std::runtime_error(SDL_GetError());
 
@@ -32,6 +31,13 @@ void Display::Draw(const std::string& content) {
     
     SDL_RenderPresent(renderer);
 }
+
+void Display::SetWidth(int width) { m_UI_Element.SetWidth(width); }
+void Display::SetHeight(int height) { m_UI_Element.SetHeight(height); }
+void Display::SetPosition(float x, float y) { m_UI_Element.SetPosition(x, y); }
+void Display::SetBackgroundColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) { m_UI_Element.SetBackgroundColor(r, g, b, a); }
+void Display::SetContentColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) { m_UI_Element.SetContentColor(r, g, b, a); }
+void Display::SetBorderColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) { m_UI_Element.SetBorderColor(r, g, b, a); }
 
 // uint32_t color = 0xFF;
 // color = color << 8;
